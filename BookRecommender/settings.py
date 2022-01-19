06dcd1dc4d8b44e2847c011ed174c96cd71170a9
Christine Roethel <roethel.christine@gmail.com>
2022-01-19 17:44:02 -0500

    settings update

--- BookRecommender/settings.py
+++ BookRecommender/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # BASE_DIR = Path(__file__).resolve().parent.parent
     # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    # dotenv_file = os.path.join(BASE_DIR, ".env")
-    # if os.path.isfile(dotenv_file):
-    #     dotenv.load_dotenv(dotenv_file)
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
